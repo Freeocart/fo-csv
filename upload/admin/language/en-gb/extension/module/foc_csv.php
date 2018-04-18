fcceb281a4d7379ee9c86eb47be8b03e1ec1fae4
ikenfin <ikenfin@gmail.com>
2018-04-18 21:40:11 +0400

    Correct extension module

--- upload/admin/language/en-gb/extension/module/foc_csv.php
+++ upload/admin/language/en-gb/extension/module/foc_csv.php
@@ -1,2 +1,3 @@
 <?php
-$_['heading_title'] = 'FOC CSV';
+$_['heading_title'] = 'FO_CSV - Flexible import data from CSV';
+$_['text_extension']   = 'Extensions';
--- upload/admin/language/ru-ru/extension/module/foc_csv.php
+++ upload/admin/language/ru-ru/extension/module/foc_csv.php
@@ -1,2 +1,3 @@
 <?php
-$_['heading_title'] = 'FOC CSV';
+$_['heading_title'] = 'FO_CSV - гибкий импорт данных из CSV';
+$_['text_extension']   = 'Расширения';
