7db13fd24849109fea765929c3710d71db1f55e1
ikenfin <ikenfin@gmail.com>
2018-05-04 02:57:22 +0400

    :art: repainted start btn

--- frontend/foc_csv/src/components/Import.vue
+++ frontend/foc_csv/src/components/Import.vue
@@ -13,7 +13,7 @@
     <div class="row">
       <div class="col-md-12">
         <div class="form-group text-right">
-          <button @click.prevent="submitImportData" class="btn btn-primary btn-lg"><i class="fa fa-rocket"></i> {{ $t('Start import!') }}</button>
+          <button @click.prevent="submitImportData" class="btn btn-warning btn-lg"><i class="fa fa-rocket"></i> {{ $t('Start import!') }}</button>
         </div>
       </div>
       <div class="col-md-4">
