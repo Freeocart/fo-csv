c721e3de3a6c10ded9efad505607bc2894a2047f
ikenfin <ikenfin@gmail.com>
2018-05-04 00:09:09 +0400

    renamed

--- upload/admin/language/en-gb/extension/module/foc_attribute_parsers.php
+++ upload/admin/language/en-gb/extension/module/foc_attribute_parsers.php
@@ -1,5 +1,5 @@
 <?php
-$_['parser_advantshop'] = 'Атрибуты из Advantshop';
+$_['parser_advantshop'] = 'Атрибуты в формате Advantshop';
 $_['parser_advantshop_keyvalue_delimiter'] = 'Символ разделитель ключей и значений';
 $_['parser_advantshop_entries_delimiter'] = 'Символ разделитель атрибутов';
 
