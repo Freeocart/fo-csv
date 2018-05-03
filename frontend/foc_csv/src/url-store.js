b9964c7f670da129b282ea9cb92f282340488a01
ikenfin <ikenfin@gmail.com>
2018-05-04 00:08:56 +0400

    removed unnecessary code

--- frontend/foc_csv/src/url-store.js
+++ frontend/foc_csv/src/url-store.js
@@ -4,31 +4,6 @@ export default class UrlStore {
   }
 
   actionUrl (action) {
-    console.log(this._config)
     return `${this._config.baseUrl}${this._config.baseRoute}/${action}&${this._config.tokenName}=${this._config.token}`
   }
 }
-
-// import { Vuex } from 'vuex'
-
-// export default function (config) {
-//   // let urlStore = Vuex.Store({
-//   //   state: {
-//   //     ...config
-//   //   },
-//   //   getters: {
-//   //     baseUrl (state) {
-//   //       return state.baseUrl
-//   //     },
-//   //     baseRoute (state) {
-//   //       return state.baseRoute
-//   //     },
-//   //     token (state) {
-//   //       return state.token
-//   //     }
-//   //   }
-//   // })
-
-//   // return urlStore
-//   class
-// }
