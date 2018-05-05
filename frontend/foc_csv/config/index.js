f47e1e23d56aa5a2073a5f75b37b56b76dabf8b0
ikenfin <ikenfin@gmail.com>
2018-05-06 01:55:52 +0400

    proxy config

--- frontend/foc_csv/config/index.js
+++ frontend/foc_csv/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
 
     proxyTable: {
       '/': {
-        target: 'http://oc3-test.local/test.php?',
+        target: 'http://oc2-test.local:80/',
         changeOrigin: true
       }
     },
@@ -21,7 +21,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
