b8894a1ab870dcaecc534e55ba3bc864f737a270
ikenfin <ikenfin@gmail.com>
2018-05-06 23:58:36 +0400

    added column attribute parser language

--- upload/admin/language/ru-ru/extension/module/foc_attribute_parsers.php
+++ upload/admin/language/ru-ru/extension/module/foc_attribute_parsers.php
@@ -3,4 +3,6 @@ $_['parser_advantshop'] = 'Атрибуты в формате Advantshop';
 $_['parser_advantshop_keyvalue_delimiter'] = 'Символ разделитель ключей и значений';
 $_['parser_advantshop_entries_delimiter'] = 'Символ разделитель атрибутов';
 
+$_['parser_column'] = 'Атрибуты в колонках';
+$_['parser_column_column'] = 'Колонка';
 /* CUSTOM PARSER */
