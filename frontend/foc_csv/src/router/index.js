e96547b260440626a7b3f9248d7e2426240ac509
ikenfin <ikenfin@gmail.com>
2018-05-07 02:32:08 +0400

    exporter path fix

--- frontend/foc_csv/src/router/index.js
+++ frontend/foc_csv/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Import from '@/components/importer/Import'
-import Export from '@/components/Export'
+import Export from '@/components/exporter/Export'
 import BackupRestore from '@/components/util/BackupRestore'
 
 Vue.use(Router)
