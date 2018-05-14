424ea376d69ce35ecd1dcdc4afa048ab6ec9c505
ikenfin <ikenfin@gmail.com>
2018-05-15 03:18:45 +0400

    connected exporter vuex module

--- frontend/foc_csv/src/store/index.js
+++ frontend/foc_csv/src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import importer from './importer'
+import exporter from './exporter'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    importer
+    importer,
+    exporter
   }
 })
 
