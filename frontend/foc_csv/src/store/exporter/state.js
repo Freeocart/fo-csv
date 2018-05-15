4641e95ed272aa1efb62c794a4a55a33fa1cf5f2
ikenfin <ikenfin@gmail.com>
2018-05-16 01:36:12 +0400

    export progress model

--- frontend/foc_csv/src/store/exporter/state.js
+++ frontend/foc_csv/src/store/exporter/state.js
@@ -3,5 +3,10 @@ import { DEFAULT_PROFILE_NAME } from '@/config'
 export default {
   data: {},
   profile: {},
+  exportJob: {
+    exportJobWorking: false,
+    exportJobCurrent: 0,
+    exportJobTotal: 0
+  },
   currentProfile: DEFAULT_PROFILE_NAME
 }
