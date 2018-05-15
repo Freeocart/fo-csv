f19a3d04a15d9dda32953ed2b9d28f05ff66331c
ikenfin <ikenfin@gmail.com>
2018-05-15 19:00:12 +0400

    common routes for export and import

--- frontend/foc_csv/src/api/modules/exporter.js
+++ frontend/foc_csv/src/api/modules/exporter.js
@@ -1,3 +1,10 @@
-export default {
+import Vue from 'vue'
+import {
+  SAVE_PROFILE_URL
+} from '@/api/routes'
 
+export default {
+  async saveProfile (mkUrl, options) {
+    return Vue.http.post(mkUrl(SAVE_PROFILE_URL), options)
+  }
 }
--- frontend/foc_csv/src/api/modules/importer.js
+++ frontend/foc_csv/src/api/modules/importer.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 
 import {
   IMPORT_URL,
-  SAVE_IMPORT_PROFILE_URL,
+  SAVE_PROFILE_URL,
   SAVE_ALL_IMPORT_PROFILES_URL
 } from '@/api/routes'
 
 export default {
 
   async saveProfile (mkUrl, options) {
-    return Vue.http.post(mkUrl(SAVE_IMPORT_PROFILE_URL), options)
+    return Vue.http.post(mkUrl(SAVE_PROFILE_URL), options)
   },
 
   async saveProfiles (mkUrl, profiles) {
