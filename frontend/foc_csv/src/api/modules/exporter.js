7720beda211bccd10f2642188111d6231c602f20
ikenfin <ikenfin@gmail.com>
2018-05-16 01:33:56 +0400

    frontend export api methods

--- frontend/foc_csv/src/api/modules/exporter.js
+++ frontend/foc_csv/src/api/modules/exporter.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import {
+  EXPORT_URL,
   SAVE_PROFILE_URL
 } from '@/api/routes'
 
 export default {
   async saveProfile (mkUrl, options) {
     return Vue.http.post(mkUrl(SAVE_PROFILE_URL), options)
+  },
+
+  async submitData (mkUrl, profile) {
+    let request = new FormData()
+    request.append('profile-json', JSON.stringify(profile))
+
+    return Vue.http.post(mkUrl(EXPORT_URL), request, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
   }
 }
