4345d17602c0946c9a02372e08cb65ae1750a604
ikenfin <ikenfin@gmail.com>
2018-05-16 18:34:15 +0400

    :speech_balloon: added translations for frontend preload texts

--- upload/admin/language/en-gb/extension/module/foc_csv.php
+++ upload/admin/language/en-gb/extension/module/foc_csv.php
@@ -1,3 +1,6 @@
 <?php
 $_['heading_title'] = 'FO_CSV - Flexible import data from CSV';
 $_['text_extension']   = 'Extensions';
+
+$_['foc_app_preload_title'] = 'Loading...';
+$_['foc_app_preload_description'] = 'If an application takes too long and not working, please clear your browser cache and refresh this page. If it wasnt helpful, please contact with maintainer - <a href="https://ikfi.ru" target="_blank">site</a>, <a href="mailto:ikenfin@gmail.com?subject=FOC_CSV%20Support%20(loading%20error)%20[eng]">e-mail</a>';
