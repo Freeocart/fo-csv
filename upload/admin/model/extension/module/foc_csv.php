7a41ff45e0d0e571669c135cd9222a2597f70035
ikenfin <ikenfin@gmail.com>
2018-06-04 19:29:35 +0400

    safe product edit method - prevent remove data before check user input

--- upload/admin/model/extension/module/foc_csv.php
+++ upload/admin/model/extension/module/foc_csv.php
@@ -109,6 +109,8 @@ class ModelExtensionModuleFocCsv extends ModelExtensionModuleFocCsvCommon {
     $tableData[$this->language_id] = array_replace(array(
       'description' => '',
       'meta_title' => '',
+      // ocstore compat
+      'meta_h1' => '',
       'tag' => '',
       'name' => '',
       'meta_description' => '',
@@ -640,7 +642,7 @@ class ModelExtensionModuleFocCsv extends ModelExtensionModuleFocCsvCommon {
 
     if (!$this->checkBeforeInsert || $this->checkerValue === ($id > 0)) {
       if ($this->updateExisting) {
-        $this->model_catalog_product->editProduct($id, $fields);
+        $this->safeEditProduct($id, $fields);
         return $id;
       }
       if ($this->deleteMode) {
@@ -655,6 +657,108 @@ class ModelExtensionModuleFocCsv extends ModelExtensionModuleFocCsvCommon {
     if ($this->insertNew) {
       return $this->model_catalog_product->addProduct($fields);
     }
+  }
+
+  /*
+    Opencart default product models delete all data before check user input
+    Also many modules can modify default model, so we want only update our existing fields
+    Unfortunatenally we need to duplicate all this code here
+    Only FO CSV supported fields being updated!
+  */
+  public function safeEditProduct ($product_id, $data) {
+
+    // we do not want to lose product data before import, so use old data as basement
+    $product_data = $this->model_catalog_product->getProduct($product_id);
+    $data = array_merge($data, $product_data);
+
+    // update product table
+    $this->db->query("UPDATE " . DB_PREFIX . "product SET model = '" . $this->db->escape($data['model']) . "', sku = '" . $this->db->escape($data['sku']) . "', upc = '" . $this->db->escape($data['upc']) . "', ean = '" . $this->db->escape($data['ean']) . "', jan = '" . $this->db->escape($data['jan']) . "', isbn = '" . $this->db->escape($data['isbn']) . "', mpn = '" . $this->db->escape($data['mpn']) . "', location = '" . $this->db->escape($data['location']) . "', quantity = '" . (int)$data['quantity'] . "', minimum = '" . (int)$data['minimum'] . "', subtract = '" . (int)$data['subtract'] . "', stock_status_id = '" . (int)$data['stock_status_id'] . "', date_available = '" . $this->db->escape($data['date_available']) . "', manufacturer_id = '" . (int)$data['manufacturer_id'] . "', shipping = '" . (int)$data['shipping'] . "', price = '" . (float)$data['price'] . "', points = '" . (int)$data['points'] . "', weight = '" . (float)$data['weight'] . "', weight_class_id = '" . (int)$data['weight_class_id'] . "', length = '" . (float)$data['length'] . "', width = '" . (float)$data['width'] . "', height = '" . (float)$data['height'] . "', length_class_id = '" . (int)$data['length_class_id'] . "', status = '" . (int)$data['status'] . "', tax_class_id = '" . (int)$data['tax_class_id'] . "', sort_order = '" . (int)$data['sort_order'] . "', date_modified = NOW() WHERE product_id = '" . (int)$product_id . "'");
+
+    if (isset($data['image'])) {
+			$this->db->query("UPDATE " . DB_PREFIX . "product SET image = '" . $this->db->escape($data['image']) . "' WHERE product_id = '" . (int)$product_id . "'");
+    }
+
+    // description
+    if (isset($data['product_description']) && !empty($data['product_description'])) {
+      $this->db->query("DELETE FROM " . DB_PREFIX . "product_description WHERE product_id = '" . (int)$product_id . "'");
+
+      foreach ($data['product_description'] as $language_id => $value) {
+        $this->db->query("INSERT INTO " . DB_PREFIX . "product_description SET product_id = '" . (int)$product_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', tag = '" . $this->db->escape($value['tag']) . "', meta_title = '" . $this->db->escape($value['meta_title']) . "', meta_h1 = '" . $this->db->escape($value['meta_h1']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "'");
+      }
+    }
+
+    // product_store update
+    if (isset($data['product_store']) && !empty($data['product_store'])) {
+      $this->db->query("DELETE FROM " . DB_PREFIX . "product_to_store WHERE product_id = '" . (int)$product_id . "'");
+
+      if (isset($data['product_store'])) {
+        foreach ($data['product_store'] as $store_id) {
+          $this->db->query("INSERT INTO " . DB_PREFIX . "product_to_store SET product_id = '" . (int)$product_id . "', store_id = '" . (int)$store_id . "'");
+        }
+      }
+    }
+
+    // attributes
+    if (isset($data['product_attribute']) && !empty($data['product_attribute'])) {
+      $this->db->query("DELETE FROM " . DB_PREFIX . "product_attribute WHERE product_id = '" . (int)$product_id . "'");
+
+      if (!empty($data['product_attribute'])) {
+        foreach ($data['product_attribute'] as $product_attribute) {
+          if ($product_attribute['attribute_id']) {
+            // Removes duplicates
+            $this->db->query("DELETE FROM " . DB_PREFIX . "product_attribute WHERE product_id = '" . (int)$product_id . "' AND attribute_id = '" . (int)$product_attribute['attribute_id'] . "'");
+
+            foreach ($product_attribute['product_attribute_description'] as $language_id => $product_attribute_description) {
+              $this->db->query("INSERT INTO " . DB_PREFIX . "product_attribute SET product_id = '" . (int)$product_id . "', attribute_id = '" . (int)$product_attribute['attribute_id'] . "', language_id = '" . (int)$language_id . "', text = '" .  $this->db->escape($product_attribute_description['text']) . "'");
+            }
+          }
+        }
+      }
+    }
+
+    // image
+		if (isset($data['product_image']) && !empty($data['product_image'])) {
+      $this->db->query("DELETE FROM " . DB_PREFIX . "product_image WHERE product_id = '" . (int)$product_id . "'");
+
+			foreach ($data['product_image'] as $product_image) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "product_image SET product_id = '" . (int)$product_id . "', image = '" . $this->db->escape($product_image['image']) . "', sort_order = '" . (int)$product_image['sort_order'] . "'");
+			}
+		}
+
+    // product categories
+		if (isset($data['product_category']) && !empty($data['product_category'])) {
+      $this->db->query("DELETE FROM " . DB_PREFIX . "product_to_category WHERE product_id = '" . (int)$product_id . "'");
+
+			foreach ($data['product_category'] as $category_id) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "product_to_category SET product_id = '" . (int)$product_id . "', category_id = '" . (int)$category_id . "'");
+			}
+    }
+
+    // main category
+    if(isset($data['main_category_id']) && $data['main_category_id'] > 0) {
+			$this->db->query("DELETE FROM " . DB_PREFIX . "product_to_category WHERE product_id = '" . (int)$product_id . "' AND category_id = '" . (int)$data['main_category_id'] . "'");
+			$this->db->query("INSERT INTO " . DB_PREFIX . "product_to_category SET product_id = '" . (int)$product_id . "', category_id = '" . (int)$data['main_category_id'] . "', main_category = 1");
+		} elseif(isset($data['product_category'][0])) {
+			$this->db->query("UPDATE " . DB_PREFIX . "product_to_category SET main_category = 1 WHERE product_id = '" . (int)$product_id . "' AND category_id = '" . (int)$data['product_category'][0] . "'");
+    }
+
+    // seo url
+    // oc3 uses its own `product_seo_url`, we use keyword for backward compatibility
+    if (isset($data['keyword'])) {
+      $seo_table = 'url_alias';
+
+      if ($this->isOpencart3()) {
+        $seo_table = 'seo_url';
+      }
+
+      $this->db->query("DELETE FROM " . DB_PREFIX . $seo_table . " WHERE query = 'product_id=" . (int)$product_id . "'");
+
+      if ($data['keyword']) {
+        $this->db->query("INSERT INTO " . DB_PREFIX . $seo_table . " SET query = 'product_id=" . (int)$product_id . "', keyword = '" . $this->db->escape($data['keyword']) . "'");
+      }
+    }
+
+    $this->cache->delete('product');
   }
 
   /*
