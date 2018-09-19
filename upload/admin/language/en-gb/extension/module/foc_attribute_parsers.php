7cc3502be9226625c7f1f00b279a812681328dc3
ikenfin <ikenfin@gmail.com>
2018-09-20 02:22:37 +0400

    :speech_balloon: parser_column translations

--- upload/admin/language/en-gb/extension/module/foc_attribute_parsers.php
+++ upload/admin/language/en-gb/extension/module/foc_attribute_parsers.php
@@ -3,4 +3,6 @@ $_['parser_advantshop'] = 'Advantshop attributes format';
 $_['parser_advantshop_keyvalue_delimiter'] = 'Key/value delimiter';
 $_['parser_advantshop_entries_delimiter'] = 'Entries delimiter';
 
+$_['parser_column'] = 'Column based attributes';
+$_['parser_column_column'] = 'Column';
 /* CUSTOM PARSER */
