ff2780ce1fd9aa2d90c78a0e4cc66fe01c67c150
ikenfin <ikenfin@gmail.com>
2018-10-11 22:06:49 +0400

    changed support emails

--- upload/admin/language/ru-ru/extension/module/foc_csv.php
+++ upload/admin/language/ru-ru/extension/module/foc_csv.php
@@ -3,4 +3,4 @@ $_['heading_title'] = 'FO_CSV - гибкий импорт данных из CSV'
 $_['text_extension']   = 'Расширения';
 
 $_['foc_app_preload_title'] = 'Загрузка приложения...';
-$_['foc_app_preload_description'] = 'Если приложение долго не загружается, попробуйте обновить кэш браузера и перезагрузить страницу. Если это не помогло - свяжитесь с автором - <a href="https://ikfi.ru" target="_blank">сайт</a>, <a href="mailto:ikenfin@gmail.com?subject=FOC_CSV%20Support%20(loading%20error)%20[eng]">e-mail</a>';
+$_['foc_app_preload_description'] = 'Если приложение долго не загружается, попробуйте обновить кэш браузера и перезагрузить страницу. Если это не помогло - свяжитесь с автором - <a href="https://ikfi.ru" target="_blank">сайт</a>, <a href="mailto:focsv-support@freeocart.ru?subject=FOC_CSV%20Support%20(loading%20error)%20[eng]">e-mail</a>';
