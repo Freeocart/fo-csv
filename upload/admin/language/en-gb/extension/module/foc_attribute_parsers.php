bef2ef6a55a2a2da17b311ba59f76bee0ac93698
ikenfin <ikenfin@gmail.com>
2018-12-05 03:18:44 +0400

    :speech_balloon: added translations to en

--- upload/admin/language/en-gb/extension/module/foc_attribute_parsers.php
+++ upload/admin/language/en-gb/extension/module/foc_attribute_parsers.php
@@ -3,6 +3,10 @@ $_['parser_advantshop'] = 'Advantshop attributes format';
 $_['parser_advantshop_keyvalue_delimiter'] = 'Key/value delimiter';
 $_['parser_advantshop_entries_delimiter'] = 'Entries delimiter';
 
+$_['parser_advantshop_grouped'] = 'Grouped advantshop attributes format';
+$_['parser_advantshop_grouped_groupattr_delimiter'] = 'Group and attributes delimiter';
+$_['parser_advantshop_grouped_groups_delimiter'] = 'Groups delimiter';
+
 $_['parser_column'] = 'Column based attributes';
 $_['parser_column_column'] = 'Column';
 /* CUSTOM PARSER */
