5fdf396a2770f8fe9552d4157716fb4ea59be22a
ikenfin <ikenfin@gmail.com>
2019-04-23 16:27:39 +0400

    :speech_balloon: addded translations for attribute encoders

---
+++ upload/admin/language/en-gb/extension/module/foc_attribute_encoders.php
@@ -0,0 +1,12 @@
+<?php
+$_['encoder_advantshop'] = 'Advantshop attributes format';
+$_['encoder_advantshop_keyvalue_delimiter'] = 'Key/value delimiter';
+$_['encoder_advantshop_entries_delimiter'] = 'Entries delimiter';
+
+$_['encoder_advantshop_grouped'] = 'Grouped advantshop attributes format';
+$_['encoder_advantshop_grouped_groupattr_delimiter'] = 'Group and attributes delimiter';
+$_['encoder_advantshop_grouped_groups_delimiter'] = 'Groups delimiter';
+
+$_['encoder_column'] = 'Column based attributes';
+$_['encoder_column_column'] = 'Column';
+/* CUSTOM ENCODER */
---
+++ upload/admin/language/ru-ru/extension/module/foc_attribute_encoders.php
@@ -0,0 +1,9 @@
+<?php
+$_['encoder_advantshop'] = 'Атрибуты в формате Advantshop';
+$_['encoder_advantshop_keyvalue_delimiter'] = 'Символ разделитель ключей и значений';
+$_['encoder_advantshop_entries_delimiter'] = 'Символ разделитель атрибутов';
+
+$_['encoder_advantshop_grouped'] = 'Сгруппированные атрибуты в формате Advantshop';
+$_['encoder_advantshop_grouped_groupattr_delimiter'] = 'Символ разделитель между группой и списком значений';
+$_['encoder_advantshop_grouped_groups_delimiter'] = 'Символ разделитель между группами';
+/* CUSTOM ENCODER */
