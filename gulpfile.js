25bd155a87919fb17d8e1d31b3be3111c32d5430
Sergey Tominoff <ikenfin@gmail.com>
2021-01-27 16:26:31 +0400

    :recycle: update dependencies, update gulpfile to support v4, update build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,39 @@
-const gulp = require('gulp'),
-      zip = require('gulp-zip');
+const { task, series, src, dest, watch } = require('gulp')
+const zip = require('gulp-zip')
+const { name } = require('./package.json')
 
-gulp.task('mktest', () => {
-  gulp.src('./upload/**')
-      .pipe(gulp.dest(process.env.SITE_DIR));
-});
+const BUILD_ZIP_NAME = `${name}.ocmod.zip`
+const SITE_PATH = process.env.SITE_DIR || './compiled-files'
 
-gulp.task('test', ['mktest'], () => {
-  gulp.watch('./upload/**', () => {
-    gulp.start('mktest');
-  });
-});
+// globs:
+const GLOB_INSTALL_XML = './install.xml'
+const GLOB_SOURCE_BASE = './upload/**'
+// we dont need it at now as distribution is universal
+// const SOURCE_TEMPLATES_EXCLUDE_GLOB = process.env.TARGET_PLATFORM === '2' ? `!${GLOB_SOURCE_BASE}/*.twig` : `!${GLOB_SOURCE_BASE}/*.tpl`
+const GLOB_EXCLUDES = [
+  '!**/.DS_Store',
+  '!**/.git',
+]
 
-gulp.task('build', () => {
-  gulp.src('./upload/*')
-      .pipe(zip('foc_csv.ocmod.zip'))
-      .pipe(gulp.dest('./'));
-})
+const copyFiles = () =>
+  src([ GLOB_SOURCE_BASE, ...GLOB_EXCLUDES ])
+    .pipe(dest(SITE_PATH))
+
+const initWatcher = () =>
+  watch([ GLOB_SOURCE_BASE, ...GLOB_EXCLUDES ], copyFiles)
+
+const packFiles = () =>
+  src([ GLOB_SOURCE_BASE, GLOB_INSTALL_XML, ...GLOB_EXCLUDES ], { base: '.' })
+    .pipe(zip(BUILD_ZIP_NAME))
+    .pipe(dest('./'))
+
+/*
+  Tasks:
+*/
+task('pack:files', packFiles)
+
+task('copy:files', copyFiles)
+
+task('watch:files', series(copyFiles, initWatcher))
+
+task('build', packFiles)
--- pack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-out="foc_csv.ocmod.zip"
-[ -f "$out" ] && rm "$out"
-zip -r9 --exclude=*.git* --exclude=*.DS_Store* "$out" ./upload ./install.xml
